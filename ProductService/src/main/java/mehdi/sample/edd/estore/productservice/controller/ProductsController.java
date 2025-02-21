b7eaab0d660c7e03453e86d6c0c3939441c64968
Mahdi Shivaeifar <shivaeifar.m@gmail.com>
2025-02-21 13:53:04 +0330

    implement command and event for product creation

--- ProductService/src/main/java/mehdi/sample/edd/estore/productservice/controller/ProductsController.java
+++ ProductService/src/main/java/mehdi/sample/edd/estore/productservice/controller/ProductsController.java
@@ -1,35 +1,55 @@
 package mehdi.sample.edd.estore.productservice.controller;
 
+import mehdi.sample.edd.estore.productservice.command.CreateProductCommand;
+import mehdi.sample.edd.estore.productservice.model.Product;
+import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/products") // http://localhost:8080/products
 public class ProductsController {
 
     private final Environment environment;
+    private final CommandGateway commandGateway;
 
-    public ProductsController(Environment environment) {
+    public ProductsController(Environment environment, CommandGateway commandGateway) {
         this.environment = environment;
+        this.commandGateway = commandGateway;
     }
 
     @PostMapping
-    public String createProduct(){
-        return "Http Post Handed";
+    public String createProduct(@RequestBody Product product) {
+
+        CreateProductCommand createProductCommand = CreateProductCommand.builder()
+                .title(product.title())
+                .price(product.price())
+                .quantity(product.quantity())
+                .productId(UUID.randomUUID().toString()).build();
+
+        String returnValue;
+        try {
+            returnValue = commandGateway.sendAndWait(createProductCommand);
+        } catch (Exception e) {
+            returnValue = e.getLocalizedMessage();
+        }
+        return returnValue;
     }
 
     @GetMapping
-    public String getProduct(){
+    public String getProduct() {
         return "Http Get Handled on service port " + environment.getProperty("local.server.port");
     }
 
     @PutMapping
-    public String updateProduct(){
+    public String updateProduct() {
         return "Http Put Handed";
     }
 
     @DeleteMapping
-    public String deleteProduct(){
+    public String deleteProduct() {
         return "Http Delete Handed";
     }
 }
---
+++ ProductService/src/main/java/mehdi/sample/edd/estore/productservice/model/Product.java
@@ -0,0 +1,7 @@
+package mehdi.sample.edd.estore.productservice.model;
+
+import java.math.BigDecimal;
+
+public record Product(String title,BigDecimal price,Integer quantity){
+
+}
