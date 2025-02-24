e18de7d6fdaed76c5e949ec920db9782c9703bbd
Mahdi Shivaeifar <shivaeifar.m@gmail.com>
2025-02-24 10:33:45 +0330

    init orders service module

---
+++ OrdersService/src/test/java/mehdi/sample/edd/estore/ordersservice/OrdersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mehdi.sample.edd.estore.ordersservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
